d56a572716e66d7fe6b1b5249da4518d6ee057ba
JaafarHady <jaafarhady5@gmail.com>
2020-09-14 23:36:34 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,7 +22,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 def to_degrees(angle_rads: float) -> tuple:
     a= 
-    return 180/math.pi*angle_rads, (180/math.pi*angle_rads)*60, ((180/math.pi*angle_rads)*60)*60
+    return 180/math.pi*angle_rads, (180/math.pi*angle_rads)*60, ((180/math.pi*angle_rads)*3600
 
 
 def to_celsius(temperature: float) -> float:
